79b1681c4f0702e485582798d6a9cb7859cdf42b
Kyle Thomsen <kthomsen24@gmail.com>
2013-09-29 03:42:14 -0500

    updated to use numbers from figure 3.3

--- Problem 3.15/prob_3_15_KYLE.cpp
+++ Problem 3.15/prob_3_15_KYLE.cpp
@@ -3,71 +3,66 @@
 
 using namespace std;
 
-double period[6],		// periods in years
-       dist[6],			// relative distances in AU
-       l_period[6],		// natural logs of periods
-       l_dist[6],		// natural logs of distances
-       m,			// y-intercept of the linear fit
-       n,			// slope of the linear fit; also the index of interest in this problem
-       sum_l_dist = 0,		// sum of l_dist
-       sum_l_dist_2 = 0,	// sum of l_dist^2
-       sum_l_period = 0,	// sum of l_period
-       sum_l_period_dist = 0;	// sum of l_dist * l_period
+double velocity[11],            // velocities
+       t[11],                   // times
+       m,                       // y-intercept of the linear fit
+       n,                       // slope of the linear fit; also the index of interest in this problem
+       sum_t = 0,               // sum of t
+       sum_t_2 = 0,             // sum of t^2
+       sum_velocity = 0,        // sum of velocities
+       sum_velocity_t = 0;      // sum of t * velocities
 
 // set the lists as noted in Table 3.2
 void initialize()
 {
-  // set the periods
-  period[0] = .242;
-  period[1] = .616;
-  period[2] = 1.000;
-  period[3] = 1.881;
-  period[4] = 11.86;
-  period[5] = 29.33;
+  // set the velocities
+  velocity[0]  = -0.10290;
+  velocity[1]  =  0.37364;
+  velocity[2]  =  2.43748;
+  velocity[3]  =  3.93836;
+  velocity[4]  =  3.31230;
+  velocity[5]  =  5.49472;
+  velocity[6]  =  5.43325;
+  velocity[7]  =  6.39321;
+  velocity[8]  =  9.06048;
+  velocity[9]  =  9.36416;
+  velocity[10] =  9.52066;
   
-  // set the distances
-  dist[0] = .388;
-  dist[1] = .724;
-  dist[2] = 1.000;
-  dist[3] = 1.524;
-  dist[4] = 5.200;
-  dist[5] = 9.510;
-}
-
-// take the natural logs of the given list of values
-void logarithms()
-{
-  int counter;
-  
-  for (counter = 0; counter < 6; counter++)
-  {
-    l_period[counter] = log(period[counter]);
-    l_dist[counter]   = log(dist[counter]);
-  }
+  // set the times
+  t[0]  = 0.00;
+  t[1]  = 0.10;
+  t[2]  = 0.20;
+  t[3]  = 0.30;
+  t[4]  = 0.40;
+  t[5]  = 0.50;
+  t[6]  = 0.60;
+  t[7]  = 0.70;
+  t[8]  = 0.80;
+  t[9]  = 0.90;
+  t[10] = 1.00;
 }
 
 void summations()
 {
   int counter;
   
-  for (counter = 0; counter < 6; counter++)
+  for (counter = 0; counter < 11; counter++)
   {
-    sum_l_dist        += l_dist[counter];
-    sum_l_dist_2      += pow(l_dist[counter],2);
-    sum_l_period      += l_period[counter];
-    sum_l_period_dist += l_period[counter] * l_dist[counter];
+    sum_t          += t[counter];
+    sum_t_2        += pow(t[counter],2);
+    sum_velocity   += velocity[counter];
+    sum_velocity_t += velocity[counter] * t[counter];
   }
 }
 
 int main()
 {
   initialize();
-  logarithms();
   summations();
   
   // calculate slopes and intercepts using least-squares relationships
-  double slope = (sum_l_period_dist - sum_l_dist * sum_l_period / 6.0) / (sum_l_dist_2 - sum_l_dist*sum_l_dist / 6.0);
-  double intercept = (sum_l_period - sum_l_dist * slope) / 6.0;
+  double slope = (sum_velocity_t - sum_t * sum_velocity / 12.0) / (sum_t_2 - sum_t*sum_t / 12.0);
+  double intercept = (sum_velocity - sum_t * slope) / 12.0;
   
   cout << "linear fit intercept is: " << intercept << endl;
   cout << "linear fit slope is: " << slope << endl;
