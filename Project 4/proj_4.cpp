291bc5e0dc0dd82b50e6e93b17b5727ea525ea40
Kyle Thomsen <kthomsen24@gmail.com>
2013-11-15 15:40:13 -0600

    documentation overdrive version

--- Project 4/proj_4.cpp
+++ Project 4/proj_4.cpp
@@ -12,168 +12,203 @@
 
 using namespace std;
 
-typedef double F(double,double,double);        // defining a type allowing the program
+typedef double F(double,double,double); // defining a type allowing the program
                                         // to more closely parallel the book's
-                                        // notation, namely renaming
-                                        // v'(t,v) = f(t,v)
+                                        // notation
 
-vector<vector<double> > vel_results;    // vector of vectors containing the
+vector<vector<double> > v_results;      // vector of vectors containing the
                                         // results of the calculations
                                         // row 0: t values
                                         // row 1: RK4 results
 
-vector<vector<double> > pos_results;    // vector of vectors containing the
+vector<vector<double> > x_results;      // vector of vectors containing the
                                         // results of the integration
                                         // row 0: t values
-                                        // row 1: uses RK4 results
+                                        // row 1: RK4 results
 
   double h;                             // step size
-  int case_num;                         // case number for easily running
-                                        // test cases
+  int case_num;                         // case number returning different
+                                        // derivatives via prime()
   int num_iterations;                   // number of iterations
-
-// constant declarations
 
   double epsilon;                       // the van der Pol oscillator constant
 
-  double v0, x0, low, high;
+  double x0;                            // x(0)
+  double v0;                            // x'(0)
+  double low;                           // lower limit
+  double high;                          // upper limit
 
-// calculating v(t) via RK4
-void rk4(F f, double h)
+// uses the fourth-order RK method to populate x_results and v_results
+//
+// input:
+//      f - a renaming of the function input, here prime(), in order
+//          to mirror the book's notation
+// output:
+//      none
+void rk4(F f)
 {
-    double counter = 0;
-    double f0a, f1a, f2a, f3a;
-    double f0b, f1b, f2b, f3b;
+    double counter = 0;                 // a counter variable
+    double f0a, f1a, f2a, f3a;          // intermediate function values for x
+    double f0b, f1b, f2b, f3b;          // intermediate function values for v
 
 // setting the initial conditions
     double x = x0;
     double v = v0;
-    pos_results[0][0] = 0;
-    pos_results[1][0] = x;
-    vel_results[0][0] = 0;
-    vel_results[1][0] = v;
+    x_results[0][0] = 0;
+    x_results[1][0] = x;
+    v_results[0][0] = 0;
+    v_results[1][0] = v;
 
-
+// looping over time
     for (double t = 0; t < (high-low); t += h)
     {
-// intermediate function evaluations: position
+// setting case_num so that f will return the derivative of x
       case_num = 0;
+// intermediate function evaluations: x
       f0a = h * f(t, v, x);
       f1a = h * f(t + h / 2, v, x + f0a / 2);
       f2a = h * f(t + h / 2, v, x + f1a / 2);
       f3a = h * f(t + h, v, x + f2a);
+// combining the intermediate function evaluations: x
       x += (f0a + 2 * f1a + 2 * f2a + f3a) / 6;
-      pos_results[0][counter+1] = t;
-      pos_results[1][counter+1] = x;
+// setting the x results
+      x_results[0][counter+1] = t;
+      x_results[1][counter+1] = x;
 
-// intermediate function evaluations: velocity
+// setting case_num so that f will return the derivative of v
       case_num = 1;
+// intermediate function evaluations: v
       f0b = h * f(t, v, x);
       f1b = h * f(t + h / 2, v + f0b / 2, x);
       f2b = h * f(t + h / 2, v + f1b / 2, x);
       f3b = h * f(t + h, v + f2b, x);
+// combining the intermediate function evaluations: v
       v += (f0b + 2 * f1b + 2 * f2b + f3b) / 6;
-      vel_results[0][counter+1] = t;
-      vel_results[1][counter+1] = v;
-// setting result first so that the term indexed 0 will be equal to v0
-// combining the intermediate function evaluations
+// setting the v results
+      v_results[0][counter+1] = t;
+      v_results[1][counter+1] = v;
+// incrementing the counter
       counter++;}
 }
 
-// the DE expressed as an equation for the first derivative
-// inputs:
+// the two first-order DEs derived from the problem statement
+//
+// input:
 //      t - time
-//      x - position
+//      x - x
 //      v - x'
-double v_prime(double t, double v, double x)
+// output:
+//      derivative of either x or v, depending on case_num
+double prime(double t, double v, double x)
 {
 // first DE
   if(case_num == 0) {return v;}
 
 // second DE
-  if(case_num == 1) {return -x - epsilon * (pow(x,2) -1) * v;}
+  if(case_num == 1) {return -x - epsilon * (pow(x,2) - 1) * v;}
 }
 
 // create output files for making graphs with gnuplot
+//
+// input:
+//      none
+// output:
+//      none
 void print_gnuplot()
 {
-// setting up velocity output files
+// setting up x output files
+  fstream f_rk4_pos;
+  f_rk4_pos.open("rk4_pos.dat", ios::out);
+
+// setting up v output files
   fstream f_rk4_vel;
   f_rk4_vel.open("rk4_vel.dat", ios::out);
 
-// setting up position output files
-  fstream f_rk4_pos;
-  f_rk4_pos.open("rk4_pos.dat", ios::out);
-  
-// setting up phase space output files
+// setting up 2D phase space output files
   fstream f_rk4_phase_2d;
   f_rk4_phase_2d.open("rk4_phase_2d.dat", ios::out);
-  
-// setting up phase space output files
+
+// setting up 3D phase space output files
   fstream f_rk4_phase_3d;
   f_rk4_phase_3d.open("rk4_phase_3d.dat", ios::out);
 
 // outputting RK4 results to files
   for (int counter = low; counter < num_iterations; counter++)
     {
-      f_rk4_vel << vel_results[0][counter] << "" <<  ","
-      <<" " << vel_results[1][counter] << endl;
-      f_rk4_pos << pos_results[0][counter] << "" <<  ","
-      <<" " << pos_results[1][counter] << endl;
-      f_rk4_phase_2d << pos_results[1][counter]<< "" <<  ","
-      <<" " << vel_results[1][counter] << endl;
-      f_rk4_phase_3d << pos_results[0][counter]<< "" <<  ","
-      <<" " << pos_results[1][counter]<< "" <<  ","
-      <<" " << vel_results[1][counter] << endl;
+// for x vs t plot: (t, x)
+      f_rk4_pos << x_results[0][counter] << "" <<  ","
+      <<" " << x_results[1][counter] << endl;
+// for x' vs t plot: (t, x')
+      f_rk4_vel << v_results[0][counter] << "" <<  ","
+      <<" " << v_results[1][counter] << endl;
+// for x vs x' plot: (x, x')
+      f_rk4_phase_2d << x_results[1][counter]<< "" <<  ","
+      <<" " << v_results[1][counter] << endl;
+// for t vs x vs x' plot: (t, x, x')
+      f_rk4_phase_3d << x_results[0][counter]<< "" <<  ","
+      <<" " << x_results[1][counter]<< "" <<  ","
+      <<" " << v_results[1][counter] << endl;
     }
 
-// close files
+// closing the created files
   f_rk4_vel.close();
   f_rk4_pos.close();
+  f_rk4_phase_2d.close();
+  f_rk4_phase_3d.close();
 }
 
 // calling the functions necessary to do the calculations
+//
+// input:
+//      none
+// output:
+//      none
 void computations()
 {
 // resize the results vectors based on the step size
     for (int counter = 0; counter < 2; counter++)
-      {vel_results[counter].resize((num_iterations+1),0.0);
-       pos_results[counter].resize((num_iterations+1),0.0);}
+      {v_results[counter].resize((num_iterations+1),0.0);
+       x_results[counter].resize((num_iterations+1),0.0);}
 // RK4 calculation
-    rk4(v_prime, h);
+    rk4(prime);
 // output gnuplot files for streamlined plotting of results
     print_gnuplot();
 }
 
 int main()
 {
-// setting the step size for the RK method
-  h = .01;
-  cout << "h = " << h << endl;
+// setting the lower and upper limits
+      low = 0;                          // lower limit
+      high = 8 * (2.0e0 * acos(0));     // upper limit
 
-      low = 0;                          // lower limit in s
-      high = 8 * (2.0e0 * acos(0));     // upper limit in s
+// user input for the step size
+      cout << "What step size?\n";
+      cout << "Recall that the interval of interest is t:[0,8π].\n";
+      cin >> h;
+      cout << endl;
+
+// calculate the number of iterations to be done
       num_iterations = static_cast<int>(abs((high-low)/h));
 
-      cout << "What value for ε?";
+// user input for ε
+      cout << "What value for ε?\n";
       cin >> epsilon;
       cout << endl;
 
-      cout << "What value for x(0)?";
+// user input for x(0)
+      cout << "What value for x(0)?\n";
       cin >> x0;
       cout << endl;
 
-      cout << "What value for x'(0)?";
+// user input for x'(0)
+      cout << "What value for x'(0)?\n";
       cin >> v0;
       cout << endl;
 
-
 // resize vectors to allow for independent variable values and two different
 // methods of solving the given DE
-  vel_results.resize(2);
-  pos_results.resize(2);
-  case_num = 0;
-  computations();
-  case_num = 1;
+  v_results.resize(2);
+  x_results.resize(2);
+// use a function to perform the calculations
   computations();
 }
