f7c023ac36d3ab6411cc1ca951dd14f1fd4727b8
Kyle Thomsen <kthomsen24@gmail.com>
2013-11-17 23:07:09 -0600

    Delete kyle.h

--- Collaborative Code Practice/kyle.h
+++
@@ -1,7 +0,0 @@
-#ifndef KYLE // this just tells the compiler to only do the stuff here if KYLE isn't defined
-#define KYLE // defining KYLE here ensures that this can't be included more than once and try
-             // to redefine the same funciton (that'd make the compiler bitch at us a lot)
-
-  double func_kyle(double x); // prototype function declared
-
-#endif
