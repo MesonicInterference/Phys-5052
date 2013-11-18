b99db5b64e0400b415d6f54f4a437c66eef6beb3
Kyle Thomsen <kthomsen24@gmail.com>
2013-11-17 23:07:04 -0600

    Delete kyle.cpp

--- Collaborative Code Practice/kyle.cpp
+++
@@ -1,8 +0,0 @@
-#include <cmath>
-double func_kyle(double x) // my actual function
-{
-  x = sin(x);   // have to include cmath here as well if I want to use a sine
-  x = pow(x,2);
-  x = x - 1;
-  return x;
-}
